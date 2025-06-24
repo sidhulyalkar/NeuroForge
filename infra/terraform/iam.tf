dfcf56e6ef9d0cfc2fa67829496af56a591cca4d
sidhulyalkar <sid.soccer.21@gmail.com>
2025-06-23 19:10:49 -0700

    fix(infra): update iam policies

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -1,10 +1,11 @@
 // terraform/iam.tf
-# CodeBuild Role Trust (OIDC) and Policy
+
+# OIDC trust for GitHub Actions
 
 data "aws_iam_policy_document" "codebuild_assume" {
   statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect    = "Allow"
+    actions   = ["sts:AssumeRoleWithWebIdentity"]
     principals {
       type        = "Federated"
       identifiers = [aws_iam_openid_connect_provider.github.arn]
@@ -14,6 +15,11 @@ data "aws_iam_policy_document" "codebuild_assume" {
       variable = "token.actions.githubusercontent.com:aud"
       values   = ["sts.amazonaws.com"]
     }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.project_prefix}/*:ref:refs/heads/*"]
+    }
   }
 }
 
@@ -22,15 +28,15 @@ resource "aws_iam_role" "codebuild" {
   assume_role_policy = data.aws_iam_policy_document.codebuild_assume.json
 }
 
-resource "aws_iam_role_policy" "codebuild_policy" {
-  name   = "${var.project_prefix}-codebuild-policy"
-  role   = aws_iam_role.codebuild.name
-  policy = jsonencode({
-    Version = "2012-10-17",
+resource "aws_iam_policy" "codebuild_ecr_s3" {
+  name        = "${var.project_prefix}-codebuild-policy"
+  description = "Access to ECR and S3 for CodeBuild"
+  policy      = jsonencode({
+    Version   = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
-        Action = [
+        Effect   = "Allow",
+        Action   = [
           "ecr:GetAuthorizationToken",
           "ecr:BatchCheckLayerAvailability",
           "ecr:GetDownloadUrlForLayer",
@@ -42,8 +48,8 @@ resource "aws_iam_role_policy" "codebuild_policy" {
         Resource = aws_ecr_repository.neuroforge.arn
       },
       {
-        Effect = "Allow",
-        Action = ["s3:GetObject", "s3:PutObject"],
+        Effect   = "Allow",
+        Action   = ["s3:GetObject", "s3:PutObject"],
         Resource = [
           "${aws_s3_bucket.data.arn}/*",
           "${aws_s3_bucket.models.arn}/*"
@@ -53,7 +59,12 @@ resource "aws_iam_role_policy" "codebuild_policy" {
   })
 }
 
-# SageMaker Role Trust and Access Policy
+resource "aws_iam_role_policy_attachment" "codebuild_attach" {
+  role       = aws_iam_role.codebuild.name
+  policy_arn = aws_iam_policy.codebuild_ecr_s3.arn
+}
+
+# SageMaker execution role trust
 
 data "aws_iam_policy_document" "sagemaker_assume" {
   statement {
@@ -71,28 +82,33 @@ resource "aws_iam_role" "sagemaker" {
   assume_role_policy = data.aws_iam_policy_document.sagemaker_assume.json
 }
 
-resource "aws_iam_role_policy" "sagemaker_policy" {
-  name   = "${var.project_prefix}-sagemaker-policy"
-  role   = aws_iam_role.sagemaker.name
-  policy = jsonencode({
-    Version = "2012-10-17",
+resource "aws_iam_policy" "sagemaker_access" {
+  name        = "${var.project_prefix}-sagemaker-policy"
+  description = "Access to S3 and ECR for SageMaker"
+  policy      = jsonencode({
+    Version   = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
-        Action = ["s3:GetObject", "s3:PutObject"],
+        Effect   = "Allow",
+        Action   = ["s3:ListBucket"],
+        Resource = [aws_s3_bucket.data.arn, aws_s3_bucket.models.arn]
+      },
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject", "s3:PutObject"],
         Resource = [
           "${aws_s3_bucket.data.arn}/*",
           "${aws_s3_bucket.models.arn}/*"
         ]
       },
       {
-        Effect = "Allow",
-        Action = ["ecr:GetAuthorizationToken"],
+        Effect   = "Allow",
+        Action   = ["ecr:GetAuthorizationToken"],
         Resource = "*"
       },
       {
-        Effect = "Allow",
-        Action = [
+        Effect   = "Allow",
+        Action   = [
           "ecr:BatchCheckLayerAvailability",
           "ecr:GetDownloadUrlForLayer",
           "ecr:BatchGetImage"
@@ -101,4 +117,9 @@ resource "aws_iam_role_policy" "sagemaker_policy" {
       }
     ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "sagemaker_attach" {
+  role       = aws_iam_role.sagemaker.name
+  policy_arn = aws_iam_policy.sagemaker_access.arn
 }
